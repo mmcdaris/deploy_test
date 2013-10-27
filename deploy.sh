dd1a4196b4eca643ba12e3b0d6ccfed8da52a325
mmcdaris <morganmcdaris@yahoo.com>
2013-10-27 16:51:34 -0700

    specify through arguments

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,9 @@
 #!/bin/sh
-
 # See http://help.airbrake.io/kb/api-2/deploy-tracking for details
 
-# iLove iOS
-API_KEY=
-ENVIRONMENT=production
-REPO=git://github.com/
+API_KEY=$2
+ENVIRONMENT=$1
+REPO=git://github.com/mmcdaris/deploy_test
 REVISION=`git log -n 1 --pretty=format:'%h' master`
 
 curl -d "api_key=${API_KEY}&deploy[rails_env]=${ENVIRONMENT}&deploy[scm_repository]=${REPO}&deploy[scm_revision]=${REVISION}&deploy[local_username]=${USER}" http://airbrake.io/deploys
